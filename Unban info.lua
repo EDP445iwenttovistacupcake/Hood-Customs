4abf398baf5cea89a5ae68a3d721b492206d88a6
HoodReligionss <83494506+EDP445iwenttovistacupcake@users.noreply.github.com>
2023-02-18 21:11:14 -0600

    Update Unban info.lua

--- Unban info.lua
+++ Unban info.lua
@@ -13,7 +13,8 @@
     Limiteds. << 15k+ 
     Adurite Giftcard << $25
     
-    **Disclaimer it is unknown when ur data will be wiped how this works is that i sent ur id to raycodex, the owner and wait for him to wipe u in roblox studio he does this on any time he wants so u could possibly be instantly unbanned depending on how good / bad ur stats are** 
+    **Disclaimer it is unknown when ur data will be wiped how this works is that i sent ur id to raycodex, the owner and wait for him to wipe u in roblox studio 
+      he does this on any time he wants so u could possibly be instantly unbanned depending on how good / bad ur stats are** 
 
     before any unban is processed u must wait for ur data wipe this included stats wipe, inventory wipe, litterally just everything
     if you want ur gun skins in ur inventory to be transferd that will cost u an extra $15 << How Does this work!? >> - u will be unbanned for a temporary time limit
