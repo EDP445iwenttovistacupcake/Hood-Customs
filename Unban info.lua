db26056495f99c8825d3095c56b9e75648f66da5
HoodReligionss <83494506+EDP445iwenttovistacupcake@users.noreply.github.com>
2023-02-18 20:46:03 -0600

    Create Unban info.lua

---
+++ Unban info.lua
@@ -0,0 +1,26 @@
+--[[
+
+    55 HoodReligions#5500
+
+    discord://-/users/936011129501655050  << copy and paste this into ur browser if u wanna dm me faster.
+    
+    Hood Customs Moderation / Unban information!
+    
+    
+    The List of payments for Unbans.
+    Cash App << $25
+    Paypal : currently down. <<
+    Limiteds. << 15k+ 
+    Adurite Giftcard << $25
+    
+    
+    before any unban is processed u must wait for ur data wipe this included stats wipe, inventory wipe, litterally just everything
+    if you want ur guns in ur inventory to be transferd that will cost u an extra $25
+
+    **Only Contact me if u have ur payment ready for an unban do not come in my dms saying "oh its 25 ok i got u next week."** <<
+    
+    My Discord tag is listed Above
+    
+    --// HoodReligions
+    
+--]]
